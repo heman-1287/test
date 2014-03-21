3ae10d739e58ce0a186c1ce4778d911673219c29
sumit <sumit.a@alive-mind.com>
2014-03-21 14:26:30 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Dattatreya</title>
+<link rel="stylesheet" href="css/dattatreya.css" type="text/css" />
+<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+<script type="text/javascript" src="engine1/jquery.js"></script>
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+</head>
+
+<body>
+<div class="popup-loader">
+	<img src="img/ajax-loader.gif"  />
+</div>
+<div class="slider-content">
+	<div align="center" style="font-family:calibrib; color:#000; vertical-align:middle; height:45px; font-size:17px; margin-top:11px; word-spacing:1px; ">Pipe Rehabilitation and Relining  |  Micro Tunneling  |  Pollution Control Equipments  |  Non Explosive Rock Blasting</div>
+</div>
+<div class="wrapper">
+    	<div class="header">
+        	<?php
+				require_once "header.php";
+			?>
+        </div>
+        <div class="container">
+        	<div class="slider">
+<!--            <img src="img/image.jpg" height="380" width="990" />
+-->            <div id="wowslider-container1" style="height:380px; width:990px;">
+            	<div class="ws_images">
+                <ul>
+                    <li><img src="data1/images/1.jpg" alt="1" title="1" id="wows1_0"/></li>
+                    <li><img src="data1/images/2.jpg" alt="2" title="2" id="wows1_1"/></li>
+                    <li><img src="data1/images/3.jpg" alt="3" title="3" id="wows1_2"/></li>
+                    <li><img src="data1/images/4.jpg" alt="4" title="4" id="wows1_3"/></li>
+                    <li><img src="data1/images/5.jpg" alt="5" title="5" id="wows1_4"/></li>
+                </ul>
+            </div>
+			</div>
+            	<img src="img/image_slider.png" height="54" width="1050" style="margin-top:-2px; margin-left:-30px;" />
+            </div>
+            <div class="content">
+            	<div class="strip">
+                	<div align="center" style="color:#464646; font-family:calibrib; font-size:30px; display:table-cell; vertical-align:middle; height:50px; margin-left:auto; margin-right:auto; text-align:center; width:990px;">Business Associates</div>
+                </div>
+                <div style="height:835px; width:990px; margin-left:auto; margin-right:auto; margin-top:50px; text-align:center; font-family:calibri; color:#464646;">
+                <div style="background-image:url(img/left_shadow.png); display:inline-block; height:258px; width:324px; float: left; margin-right:9px; cursor:pointer; " onclick="window.open('mts.php','_self')">
+                	<img src="img/mts.png" height="110" width="110" style="margin-top:7px;"/>
+                    <div class="company-name">MTS PERFORATOR</div>
+                    <div class="company-content">We do not only provide our customers with the right equipment, we provide them with the overall solution to make their projects successful.</div>
+                </div>
+                <div style="background-image:url(img/center.png); display:inline-block; height:258px; width:324px; float: left; margin-right:9px; cursor:pointer;" onclick="window.open('nuflow.php','_self')">
+                	<img src="img/nuflow.png" height="100" width="200" style="margin-top:7px;" />
+                    <div class="company-name">nuflow</div>
+                    <div class="company-content">With two specialised technologies available, Nuflow provides a cost effective solution to repair sewerage, stormwater, potable pipes or any pressure line.</div>
+                </div>
+                <div style="background-image:url(img/rigth_shadow.png); display:inline-block; height:258px; width:324px; float: left; cursor:pointer;" onclick="window.open('kapra.php','_self')">
+                	<img src="img/kapra.png" height="110" width="110" style="margin-top:7px;" />
+                    <div class="company-name">KAPRA</div>
+                    <div class="company-content">Eco Friendly Non Explosive Rock Blasting Pulse Plasma Technology for hard Rock Fragmentation from KAPRA, South Korea.</div>
+                </div>
+                <div style="background-image:url(img/left_shadow.png); display:inline-block; height:258px; width:324px; float: left; margin-right:9px; margin-top:15px; cursor:pointer;" onclick="window.open('the_hole_solution.php','_self')">
+                	<img src="img/the_hole_solution.png" height="110" width="110" style="margin-top:7px;" />
+                    <div class="company-name">the 'hole solution</div>
+                    <div class="company-content">AP/M believes that successful rehabilitation requires quality products and quality workmanship.</div>
+                </div>
+                <div style="background-image:url(img/center.png); display:inline-block; height:258px; width:324px; float: left; margin-right:9px; margin-top:15px; cursor:pointer;" onclick="window.open('primus_line.php','_self')">
+                	<img src="img/primus_line.png" height="100" width="200" style="margin-top:7px;" />
+                    <div class="company-name">PRIMUS LINE</div>
+                    <div class="company-content">Primus Line®  is a tried and tested pipeline system for trenchless rehabilitation of pressure pipes of different media, using the long pipe lining with an annulus method.</div>
+                </div>
+                <div style="background-image:url(img/rigth_shadow.png); display:inline-block; height:258px; width:324px; float: left; margin-top:15px; cursor:pointer;" onclick="window.open('emi_controls.php','_self')">
+                	<img src="img/emi.png" height="100" width="200" style="margin-top:7px;" />
+                    <div class="company-name">EMI CONTROLS</div>
+                    <div class="company-content">Dust/Odor/Smoke controller for creating Pollution free Green & Clean Environment from EMI Control, Italy.</div>
+                </div>
+                <div style="background-image:url(img/left_shadow.png); display:inline-block; height:258px; width:324px; float: left; margin-right:9px; margin-top:15px; cursor:pointer;" onclick="window.open('con_shield.php','_self')">
+                	<img src="img/conshield.jpg" height="100" width="200" style="margin-top:7px;" />
+                    <div class="company-name">Con Shield</div>
+                    <div class="company-content">ConmicShield® is a unique solution designed to stop the corrosion before it begins. </div>
+                </div>
+                <div style="background-image:url(img/center.png); display:inline-block; height:258px; width:324px; float: left; margin-right:9px; margin-top:15px; cursor:pointer;" onclick="window.open('crystal_x.php','_self')">
+                	<img src="img/crystalx.png" height="100" width="200" style="margin-top:7px;" />
+                    <div class="company-name">CRYSTAL X</div>
+                    <div class="company-content">Crystal-X, waterproofing solutions to existing or new cementitious products from ConShield, USA.</div>
+                </div>
+                <div style="background-image:url(img/rigth_shadow.png); display:inline-block; height:258px; width:324px; float: left; margin-top:15px; cursor:pointer;" onclick="window.open('rock_frac.php','_self')">
+                	<img src="img/rack_frac.png" height="100" width="200" style="margin-top:7px;" />
+                    <div class="company-name">ROCK FRAC</div>
+                    <div class="company-content">RockFrac® NEDA is environmental friendly Non-Explosive Demolition Agent, which is popular in the international quarrying and demolition market.</div>
+                </div>
+                </div>                
+            </div>
+        </div>
+        <div class="footer">
+        <?php
+				require_once "footer.php";
+			?>
+        </div>
+</div>
+</body>
+</html>
